c2a73234032aadf30805ac34707215cf9be9b92b
Trivette <joshuatrivette@gmail.com>
2015-09-09 20:36:35 -0500

    Not sure if this should be here...

---
+++ lab1/tj_lab1/WebContent/simpleEcho.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta charset="utf-8">
+<title>Echo of a form</title>
+</head>
+<body>
+<h1>Echo of a form submission</h1>
+<?php
+print_r($_POST);
+?>
+
+</body>
+</html>
