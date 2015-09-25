844b9f4d9c79b96f386c1195842533f6800310e1
Trivette <joshuatrivette@gmail.com>
2015-09-25 09:35:22 -0500

    Mvc structure commit

--- lab2/tj_lab2/WebContent/index.php
+++ lab2/tj_lab2/WebContent/index.php
@@ -1,3 +1,24 @@
 <?php
-echo "sup augustine";
+	include("includer.php");
+	
+	$url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+	
+	$urlPieces = split("/", $url);
+	
+	if(count($urlPieces) < 2)
+		$control = "none";
+	else 
+		$control = $urlPieces[2];
+	
+	switch($control){
+		case "login":
+			LoginController::run();
+			break;
+		case "signup":
+			SignupController::run();
+			break;
+		default:
+			HomeView::show();
+		
+	}
 ?>
---
+++ lab2/tj_lab2/WebContent/views/HomeView.class.php
@@ -0,0 +1,43 @@
+<?php
+class HomeView {
+  public static function show() {  
+		
+?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Hock League</title>
+	</head>
+	<body>
+	<section>
+	<nav>
+		<a href="signup">Register</a> |
+		<a href="login">Login</a> |
+		<a href="http://imightbejosh.com/ranks.html">Leaderboard</a> |
+		<a href="bet.html">Betting</a> |
+		<a href="games.html">Recent Games</a>
+	</nav>
+	<img src="resources/Drawing.png" alt="Banner">
+	</section>
+	<section>
+		<header>
+		<p>This is a web site for Hock League in Uniball.  Here you will find a leaderboard and player statistics.  You will also have the ability to bet on games here.</p>
+		</header>
+		<aside>
+			<h4>Uniball</h4>
+			<p>Uniball is an online multi-player space hockey video game.  Click <a href="http://uniballhq.com">here</a> for more information.</p>
+			<h4>Hock League</h4>
+			<p>Hock League is the name of the premier Uniball league dedicated to competitive 3v3 games on the Hockey map.</p>
+		</aside>
+	</section>
+	
+	<footer>
+	<p>Contact Information: <a href="mailto:joshuatrivette@gmail.com">joshuatrivette@gmail.com</a></p>
+	</footer>
+	</body>
+	</html>
+<?php
+  }
+}
+?>
