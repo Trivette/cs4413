cf17ac4be1c656f665c9a990a5e12c9fd843d498
Trivette <joshuatrivette@gmail.com>
2015-09-29 21:17:58 -0500

    Added Bet class, view, and controller.  Added new errors

---
+++ lab2/tj_lab2/WebContent/views/BetView.class.php
@@ -0,0 +1,19 @@
+<?php
+class BetView {
+  public static function show() {  
+		
+?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Hock League Betting</title>
+	</head>
+	<body>
+	
+	</body>
+	</html>
+<?php
+  }
+}
+?>
