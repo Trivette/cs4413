30b147a1a67548e2ef9d2d0527e3a9e745740889
Trivette <joshuatrivette@gmail.com>
2015-10-22 22:52:39 -0500

    Signup bug fix

--- lab3/tj_lab3/WebContent/controllers/SignupController.class.php
+++ lab3/tj_lab3/WebContent/controllers/SignupController.class.php
@@ -5,16 +5,21 @@ class SignupController {
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$webuser = new WebUser($_POST);
 			if ($webuser->getErrorCount() == 0){
-				//create a webuser in db
-				$id = WebUserDB::addUser($webuser);
-				if($id != 0){
-					//find the hockuser related to this dude
-					$hockusers = HockUserDB::getUsersBy('name', $webuser->getHockUser());
-					ProfileView::show($webuser, $hockusers[0]);//HomeView::show();
-				}
-				else{
-					$webuser->setError('userName', 'DBERROR_ADDWEBUSER');
+				if(!empty(WebUserDB::getUsersBy('hockName', $webuser->getHockName()))){
+					$webuser->setError('hockName', 'HOCKUSER_NAMECLAIMED');
 					SignupView::show($webuser);
+				}else {
+					//create a webuser in db
+					$id = WebUserDB::addUser($webuser);
+					if($id != 0){
+						//find the hockuser related to this dude
+						$hockusers = HockUserDB::getUsersBy('name', $webuser->getHockUser());
+						ProfileView::show($webuser, $hockusers[0]);//HomeView::show();
+					}
+					else{
+						$webuser->setError('userName', 'DBERROR_ADDWEBUSER');
+						SignupView::show($webuser);
+					}
 				}
 			}
 			else
